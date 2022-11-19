5421055adb61b9d557396cb6f890b7da8be0dcc9
gustavobruder <gugabruder@gmail.com>
2022-11-19 15:32:07 -0300

    Adicionado mais testes unitários para o trabalho 3 de grafos

--- tests/trabalho3/Trabalho3Tests.java
+++ tests/trabalho3/Trabalho3Tests.java
@@ -55,4 +55,121 @@ public class Trabalho3Tests {
 
         assertEquals(3, trab.executarKruskal());
     }
+
+    @Test
+    public void deveExecutar_AlgoritmoKruskal3() {
+        // Entrada
+        // 6 11
+        // 1 2 4
+        // 1 4 3
+        // 1 5 4
+        // 2 3 8
+        // 2 5 9
+        // 2 6 5
+        // 3 4 9
+        // 3 5 3
+        // 3 6 2
+        // 4 6 7
+        // 5 6 2
+        //
+        // Saída
+        // 15
+
+        Trabalho3 trab = new Trabalho3(6, 11);
+        trab.adicionarAresta(1, 2, 4);
+        trab.adicionarAresta(1, 4, 3);
+        trab.adicionarAresta(1, 5, 4);
+        trab.adicionarAresta(2, 3, 8);
+        trab.adicionarAresta(2, 5, 9);
+        trab.adicionarAresta(2, 6, 5);
+        trab.adicionarAresta(3, 4, 9);
+        trab.adicionarAresta(3, 5, 3);
+        trab.adicionarAresta(3, 6, 2);
+        trab.adicionarAresta(4, 6, 7);
+        trab.adicionarAresta(5, 6, 2);
+
+        assertEquals(15, trab.executarKruskal());
+    }
+
+    @Test
+    public void deveExecutar_AlgoritmoKruskal4() {
+        // Entrada
+        // 12 17
+        // 1 2 4
+        // 1 9 4
+        // 1 12 7
+        // 2 3 4
+        // 2 11 8
+        // 3 4 3
+        // 3 12 6
+        // 4 5 1
+        // 4 11 2
+        // 5 6 3
+        // 6 10 2
+        // 7 8 7
+        // 7 10 2
+        // 7 12 5
+        // 8 9 3
+        // 9 10 6
+        // 10 11 4
+        //
+        // Saída
+        // 33
+
+        Trabalho3 trab = new Trabalho3(12, 17);
+        trab.adicionarAresta(1, 2, 4);
+        trab.adicionarAresta(1, 9, 4);
+        trab.adicionarAresta(1, 12, 7);
+        trab.adicionarAresta(2, 3, 4);
+        trab.adicionarAresta(2, 11, 8);
+        trab.adicionarAresta(3, 4, 3);
+        trab.adicionarAresta(3, 12, 6);
+        trab.adicionarAresta(4, 5, 1);
+        trab.adicionarAresta(4, 11, 2);
+        trab.adicionarAresta(5, 6, 3);
+        trab.adicionarAresta(6, 10, 2);
+        trab.adicionarAresta(7, 8, 7);
+        trab.adicionarAresta(7, 10, 2);
+        trab.adicionarAresta(7, 12, 5);
+        trab.adicionarAresta(8, 9, 3);
+        trab.adicionarAresta(9, 10, 6);
+        trab.adicionarAresta(10, 11, 4);
+
+        assertEquals(33, trab.executarKruskal());
+    }
+
+    @Test
+    public void deveExecutar_AlgoritmoKruskal5() {
+        // Entrada
+        // 6 11
+        // 1 2 20
+        // 1 3 55
+        // 1 5 45
+        // 1 6 10
+        // 2 4 5
+        // 2 5 21
+        // 2 6 70
+        // 3 5 45
+        // 4 5 55
+        // 4 6 300
+        // 5 6 4
+        //
+        // Saída
+        // 84
+
+        Trabalho3 trab = new Trabalho3(6, 11);
+        trab.adicionarAresta(1, 2, 20);
+        trab.adicionarAresta(1, 3, 55);
+        trab.adicionarAresta(1, 5, 45);
+        trab.adicionarAresta(1, 6, 10);
+        trab.adicionarAresta(2, 4, 5);
+        trab.adicionarAresta(2, 5, 21);
+        trab.adicionarAresta(2, 6, 70);
+        trab.adicionarAresta(3, 5, 45);
+        trab.adicionarAresta(4, 5, 55);
+        trab.adicionarAresta(4, 6, 300);
+        trab.adicionarAresta(5, 6, 4);
+
+        assertEquals(84, trab.executarKruskal());
+    }
 }
